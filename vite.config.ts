30739afe6c443c869f18b15256f63b13ae379c8f
Pinky <Pinkysutherland@yahoo.com>
2026-01-28 16:51:42 +0800

    update port

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
     return {
       server: {
         port: 3000,
-        host: 'localhost',
+        host: '0.0.0.0',
       },
       plugins: [react()],
       define: {
